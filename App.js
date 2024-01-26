2baea49674cbcc637ef8d3763aa02a0bb771c0d9
Caroline Caleguer <carolineyamashitac@gmail.com>
2024-01-26 18:26:28 -0300

    Add emoji, edit emoji on photo and save photo + README file

--- App.js
+++ App.js
@@ -1,27 +1,36 @@
+import { useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import { useState } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { captureRef } from 'react-native-view-shot';
+import * as ImagePicker from 'expo-image-picker';
+import * as MediaLibrary from 'expo-media-library';
 
 import ImageViewer from './components/ImageViewer';
-import * as ImagePicker from 'expo-image-picker'
-import Button from './components/Button'
+import Button from './components/Button';
 import CircleButton from './components/CircleButton';
 import IconButton from './components/IconButton';
 import EmojiPicker from './components/EmojiPicker';
 import EmojiList from './components/EmojiList';
 import EmojiSticker from './components/EmojiSticker';
 
+
 const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
-    const [pickedEmoji, setPickedEmoji] = useState(null);
-
+    
     const [isModalVisible, setIsModalVisible] = useState(false)
-
     const [showAppOptions, setShowAppOptions] = useState(false)
-
+    const [pickedEmoji, setPickedEmoji] = useState(null);
     const [selectedImage, setSelectedImage] = useState(null)
 
+    const [status, requestPermission] = MediaLibrary.usePermissions()
+    const imageRef = useRef();
+
+    if (status === null) {
+        requestPermission();
+    }
+    
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
             allowsEditing: true,
@@ -49,34 +58,44 @@ export default function App() {
     }
 
     const onSaveImageAsync = async () => {
-
+        try {
+            const localUri = await captureRef(imageRef, { height: 440, quality: 1 });
+            await MediaLibrary.saveToLibraryAsync(localUri);
+            if (localUri) { alert("Saved!") }
+        } catch(e){
+            console.log(e)
+        }
     };
 
     return (
-        <View style={styles.container}>
-            <View style={styles.imageContainer}>
-                <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
-                {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
-            </View>
-            {showAppOptions ? (
-                <View style={styles.optionsContainer}>
-                    <View style={styles.optionsRow}>
-                        <IconButton icon="refresh" label="Reset" onPress={onReset} />
-                        <CircleButton onPress={onAddSticker} />
-                        <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+        <GestureHandlerRootView style={styles.container}>
+            <View style={styles.container}>
+                <View style={styles.imageContainer}>
+                    <View ref={imageRef} collapsable={false}>
+                        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
+                        {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
                     </View>
                 </View>
-            ) : (
-                <View style={styles.footerContainer}>
-                    <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-                    <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
-                </View>
-            )}
-            <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-                <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
-            </EmojiPicker>
-            <StatusBar style='auto' />
-        </View>
+                {showAppOptions ? (
+                    <View style={styles.optionsContainer}>
+                        <View style={styles.optionsRow}>
+                            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+                            <CircleButton onPress={onAddSticker} />
+                            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+                        </View>
+                    </View>
+                ) : (
+                    <View style={styles.footerContainer}>
+                        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+                        <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+                    </View>
+                )}
+                <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+                    <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+                </EmojiPicker>
+                <StatusBar style='auto' />
+            </View>
+        </GestureHandlerRootView>
     );
 }
 
---
+++ README.md
@@ -0,0 +1,19 @@
+# StickerSmash
+
+
+## Description
+
+This project is a mobile app where you select a photo from your phone galery, add an emoji to the photo (you can drag the emoji to put wherever you want in the photo and double tap to make it bigger or smaller) and then save the edited photo back to your gallery. This app was made based on get started tutorial from expo go documentation. 
+
+## How to use in localhost
+
+After downloading it you can run locally downloading the Expo Go app on your phone and then running:
+
+### `npm install`
+Install all the dependencies
+
+### `npm start or npm start -c or npm start --tunnel`
+Runs the app in the development mode.
+Scan the QRCode on your phone
+It will reload when you make changes.
+You may also see any errors in the console.
